8f1496a318426609be66cc16dd9fc7a667bcad09
RobertoZUpqroo <202200553@upqroo.edu.mx>
2024-02-15 18:35:54 -0500

    Se añade Normalizer al SCSS y se agrega Gulp Plumber

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require('sass'));
+const plumber = require('gulp-plumber');
 
 function css(done){
     src('src/scss/app.scss')    //Identifica el archivo SASS
+    .pipe(plumber())               //Compila el archivo SASS
     .pipe(sass())               //Compila el archivo SASS
     .pipe(dest('build/css'));   //Almacena en la hoja de estilo
 
@@ -10,7 +12,7 @@ function css(done){
 }
 
 function dev(done){
-    watch("src/scss/app.scss", css);
+    watch("src/scss/**/*.scss", css);
     done();
 }
 
