1830e50265ffe1aef5ce3abfb929d3b11cae7256
RobertoZUpqroo <202200553@upqroo.edu.mx>
2024-02-26 17:54:06 -0500

    Proyecto terminado | Se optimiza y se sube repositorio completo a GitHub

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ const { src, dest, watch, parallel } = require("gulp");
 // CSS
 const sass = require("gulp-sass")(require('sass'));
 const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps')
 
 // Imagenes
 const cache = require('gulp-cache')
@@ -10,10 +14,16 @@ const imagemin = require('gulp-imagemin')
 const webp = require('gulp-webp');
 const avif = require('gulp-avif');
 
+//JavaScript
+const terser = require('gulp-terser-js');
+
 function css(done){
     src('src/scss/app.scss')    //Identifica el archivo SASS
+    .pipe(sourcemaps.init())        //Mapea el codigo CSS que se compila
     .pipe(plumber())               //Compila el archivo SASS
     .pipe(sass())               //Compila el archivo SASS
+    .pipe(postcss([autoprefixer(), cssnano()]))  //Optimización de codigo
+    .pipe(sourcemaps.write('.'))  //Da referencias de dónde se encuentra cada linea de codigo en SASS y dónde modificarlo
     .pipe(dest('build/css'));   //Almacena en la hoja de estilo
 
     done(); //Callback para notificar a GULP cuando termine la función
@@ -53,13 +63,25 @@ function versionAvif(done){
     done();
 }
 
+function javascript(done) {
+    src('src/js/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(terser())
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('build/js'));
+
+    done();
+}
+
 function dev(done){
     watch("src/scss/**/*.scss", css);
+    watch("src/js/**/*.js", javascript);
     done();
 }
 
 exports.css = css;
+exports.js = javascript;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
 exports.versionAvif = versionAvif;
-exports.dev = parallel(imagenes,versionWebp, versionAvif, dev);
+exports.dev = parallel(imagenes,versionWebp, versionAvif, javascript, dev);
